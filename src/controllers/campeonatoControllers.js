d9ba1eaadd9377f0a17546a4f0784e18dc06bc64
Endelkys <endelkysanelmar2604@gmail.com>
2023-10-08 16:28:55 -0400

    Creando clase de los controladores

---
+++ src/controllers/campeonatoControllers.js
@@ -0,0 +1,59 @@
+
+
+class CampeonatoRobotica {
+    agregarModalidad() {
+        
+    }
+
+    agregarCategoriaAModalidad() {
+
+    }
+
+    registrarEquipo() {
+        
+    }
+
+    agregarPatrocinantes() {
+
+    }
+
+    editarEquipo() {
+
+    }
+
+    editarCategoria() {
+
+    }
+
+    mostrarPatrocinantes() {
+
+    }
+
+    mostrarEquipos() {
+
+    }
+
+    mostrarEquiposPorCategoria() {
+
+    }
+
+    eliminarEquipo() {
+
+    }
+
+    eliminarCategoria() {
+
+    }
+
+    eliminarEquipoPorCategoria() {
+
+    }
+
+}
+
+const campeonatoControllers = new CampeonatoRobotica();
+
+module.exports = {
+  campeonatoControllers
+}
+
--- src/index.js
+++ src/index.js
@@ -8,5 +8,4 @@ app.use(cors())
 app.use(express.json());
 
 
-
 app.listen(4000, () => console.log(`Servidor corriendo en el puerto 4000`));
